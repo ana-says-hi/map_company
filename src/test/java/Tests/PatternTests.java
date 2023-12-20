191855b017c09ce5c94c48bcde435519b657e366
dobra <dobrairina1@gmail.com>
2023-12-21 00:16:32 +0200

    testele pentru memento

--- src/test/java/Tests/PatternTests.java
+++ src/test/java/Tests/PatternTests.java
@@ -1,24 +1,27 @@
 package Tests;
-import CommandProcessPattern.OrderProcessor;
-import Domains.Order;
-import Domains.Status;
-import MementoPattern.OrderMemento;
-import Reposies.OrderRepo;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import java.sql.SQLException;
+import src.Domains.Status;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 public class PatternTests {
+    private Status Status;
+
     // OrderMementoTest
         public void testGetStateWithString() {
             OrderMemento orderMemento = new OrderMemento("Shipped");
             assertEquals("Shipped", orderMemento.getState());
         }
         public void testGetStateWithStatusObject() {
-            OrderMemento orderMemento = new OrderMemento(Status.PENDING);
+            OrderMemento orderMemento = new OrderMemento(Status.PENDING.toString());
             assertEquals("PENDING", orderMemento.getState());
         }
 
+    private class OrderMemento {
+        public OrderMemento(String shipped) {
+
+        }
+
+        public String getState() {
+            return null;
+        }
+    }
 }
